ad4ac85efa9206a99091cde2cee95eca1aee5cc7
vandana7032 <74043630+vandana7032@users.noreply.github.com>
2020-11-13 13:34:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# objects
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,35 @@
+var fixedRect, movingRect;
+var gameObject1,gameObject2,gameObject3,gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+
+  gameObject1=createSprite(100,100,50,50);
+  gameObject1.shapeColor="blue";
+  gameObject2=createSprite(200,100,50,50);
+  gameObject2.shapeColor="blue";
+  gameObject3=createSprite(300,100,50,50);
+  gameObject3.shapeColor="blue";
+  gameObject4=createSprite(400,100,50,50);
+  gameObject4.shapeColor="blue";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,gameObject1)){
+    movingRect.shapeColor = "red";
+    gameObject1.shapeColor = "red";
+  }
+  else{
+    movingRect.shapeColor = "green";
+    gameObject1.shapeColor = "green";
+  }
+  drawSprites();
+}
